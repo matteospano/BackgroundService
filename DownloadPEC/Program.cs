df50a618b07e19dd346f284a49c5763cb9a7891d
Matteo Spano <matteo.spano@techedgegroup.com>
2022-01-21 12:01:43 +0100

    programma ripulito, legge titti gli attachment e li mette in un buffer che si re-inizializza ad ogni nuova mail

--- DownloadPEC/Program.cs
+++ DownloadPEC/Program.cs
@@ -15,7 +15,7 @@ namespace TechEdge.Mail.Receiver
         public const string PECMailFileName = "postacert.eml";
         public const string ConfigFileName = "PECDownload.json";
 
-        static void Main(string[] args)
+        static void Main()
         {
             if (!File.Exists(ConfigFileName))
             {
@@ -24,77 +24,48 @@ namespace TechEdge.Mail.Receiver
             }
 
             Configuration Config = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText(ConfigFileName));
-
-            using (var pop = new Pop3Client(Config.Host, Config.UserName, Config.Password, Config.Port, Config.UseSSL))
+            //using (var pop = new ImapClient(Config.Host, Config.UserName, Config.Password, 0, Config.Port, Config.UseSSL))
+            using (Pop3Client pop = new Pop3Client(Config.Host, Config.UserName, Config.Password, Config.Port, Config.UseSSL))
             {
                 var MsgCount = pop.GetMessageCount();
-
-                Console.WriteLine($"Detected {MsgCount} messages");
-
                 for (var i = MsgCount - 1; i >= 0; i--)
                 {
                     var msg = pop.GetMessage(i, false);
-                    Console.WriteLine($"Processing Mail {msg.Subject} ....");
-
-                    var MailFolderName = string.IsNullOrEmpty(msg.Subject) ? string.Concat("Mail-", DateTime.Now.ToString("HHmmss")) : msg.Subject.Substring(0, Math.Min(msg.Subject.Length, 255));
-                    MailFolderName = Regex.Replace(MailFolderName, @"[^0-9a-zA-Z]+", " ");
+                    byte[] Buffer = new byte[0];
 
                     foreach (var item in msg.Attachments)
                     {
                         if (string.Compare(PECMailFileName, item.Filename, true) != 0)
                             continue;
 
-                        Console.WriteLine($"Create FileStream {item.Filename}");
-                        using (FileStream SW = new FileStream(MailFolderName + item.Filename, FileMode.CreateNew))
-                        {
-                            var Buffer = item.GetData();
-                            SW.Write(Buffer, 0, Buffer.Length);
-                        }
+                        Buffer = Buffer.Concat(item.GetData()).ToArray();
+                        Console.WriteLine($"LOG: {msg.Date},{msg.Subject},{PECMailFileName}"); //data,titolo,nomeFile
                     }
 
-                    //string MailFile = Path.Combine(MailFolder, PECMailFileName);
-                    //if (File.Exists(MailFile))
-                    //{
-                    //    using (StreamReader SR = new StreamReader(MailFile))
-                    //    {
-                    //        MailMessage M = MailMessage.FromStream(SR);
-                    //        foreach (var fileMessage in M.Attachments)
-                    //        {
-                    //            var FName = fileMessage.FName;
-                    //            if (FName.StartsWith(FileFormatToken))
-                    //            {
-                    //                FName = FName.Replace(FileFormatToken, string.Empty);
-                    //                if (FName.EndsWith("="))
-                    //                    FName = FName.Substring(0, FName.Length - 1);
+                    MemoryStream stream = new MemoryStream(Buffer);
+                    StreamReader SR = new StreamReader(stream);
+                    MailMessage M = MailMessage.FromStream(SR);
+                    foreach (var fileMessage in M.Attachments)
+                    {
+                        var FName = fileMessage.FName;
+                        if (FName.StartsWith(FileFormatToken))
+                        {
+                            FName = FName.Replace(FileFormatToken, string.Empty);
+                            if (FName.EndsWith("="))
+                                FName = FName.Substring(0, FName.Length - 1);
 
-                    //                    FName = FName.
-                    //                            Replace("=", "%").
-                    //                            Replace("?", string.Empty);
+                            FName = FName.
+                                    Replace("=", "%").
+                                    Replace("?", string.Empty);
 
-                    //                FName = Uri.UnescapeDataString(FName);
-                    //            }
-
-                    //            FName = Path.Combine(MailFolder, FName);
-                    //            if (File.Exists(FName))
-                    //                File.Delete(FName);
-
-                    //            using(FileStream FS = new FileStream(FName, FileMode.CreateNew, FileAccess.Write))
-                    //            {
-                    //                var Buffer = Convert.FromBase64String(fileMessage.FContent);
-                    //                FS.Write(Buffer, 0, Buffer.Length);
-                    //            }
-                    //        }
-                    //    }
-                    //}
-
-                    //Console.WriteLine($"Processing Mail {msg.Subject} Done");
-                    //if (Config.DeleteMessages)
-                    //{
-                    //    Console.WriteLine($"Deleting Message ${msg.Subject}");
-                    //    pop.DeleteMessage(i);
-                    //}
+                            FName = Uri.UnescapeDataString(FName);
+                        }
+                        Console.WriteLine($"{FName} read succesfully");
+                    }
                 }
             }
+            Console.WriteLine("Process end");
+            Console.ReadKey();
         }
     }
 }
