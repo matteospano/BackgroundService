10365be380191efb01f2b845a321aa4938988f17
Matteo Spano <matteo.spano@techedgegroup.com>
2022-01-21 12:08:58 +0100

    buffer unico per tutte le email, ritorna correttamente i singoli allegati

--- DownloadPEC/Program.cs
+++ DownloadPEC/Program.cs
@@ -23,6 +23,7 @@ namespace TechEdge.Mail.Receiver
                 return;
             }
 
+            byte[] Buffer = new byte[0];
             Configuration Config = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText(ConfigFileName));
             //using (var pop = new ImapClient(Config.Host, Config.UserName, Config.Password, 0, Config.Port, Config.UseSSL))
             using (Pop3Client pop = new Pop3Client(Config.Host, Config.UserName, Config.Password, Config.Port, Config.UseSSL))
@@ -31,8 +32,6 @@ namespace TechEdge.Mail.Receiver
                 for (var i = MsgCount - 1; i >= 0; i--)
                 {
                     var msg = pop.GetMessage(i, false);
-                    byte[] Buffer = new byte[0];
-
                     foreach (var item in msg.Attachments)
                     {
                         if (string.Compare(PECMailFileName, item.Filename, true) != 0)
@@ -41,30 +40,54 @@ namespace TechEdge.Mail.Receiver
                         Buffer = Buffer.Concat(item.GetData()).ToArray();
                         Console.WriteLine($"LOG: {msg.Date},{msg.Subject},{PECMailFileName}"); //data,titolo,nomeFile
                     }
-
-                    MemoryStream stream = new MemoryStream(Buffer);
-                    StreamReader SR = new StreamReader(stream);
-                    MailMessage M = MailMessage.FromStream(SR);
-                    foreach (var fileMessage in M.Attachments)
-                    {
-                        var FName = fileMessage.FName;
-                        if (FName.StartsWith(FileFormatToken))
-                        {
-                            FName = FName.Replace(FileFormatToken, string.Empty);
-                            if (FName.EndsWith("="))
-                                FName = FName.Substring(0, FName.Length - 1);
-
-                            FName = FName.
-                                    Replace("=", "%").
-                                    Replace("?", string.Empty);
-
-                            FName = Uri.UnescapeDataString(FName);
-                        }
-                        Console.WriteLine($"{FName} read succesfully");
-                    }
                 }
+                //for (var i = MsgCount - 1; i >= 0; i--)
+                //{
+                //    MemoryStream stream = new MemoryStream(Buffer);
+                //    StreamReader SR = new StreamReader(stream);
+                //    MailMessage M = MailMessage.FromStream(SR);
+                //    foreach (var fileMessage in M.Attachments)
+                //    {
+                //        var FName = fileMessage.FName;
+                //        if (FName.StartsWith(FileFormatToken))
+                //        {
+                //            FName = FName.Replace(FileFormatToken, string.Empty);
+                //            if (FName.EndsWith("="))
+                //                FName = FName.Substring(0, FName.Length - 1);
+
+                //            FName = FName.
+                //                    Replace("=", "%").
+                //                    Replace("?", string.Empty);
+
+                //            FName = Uri.UnescapeDataString(FName);                            
+                //        }
+                //        Console.WriteLine($"{FName} read succesfully");
+                //    }
+                //}
             }
-            Console.WriteLine("Process end");
+            Console.WriteLine("---------------------------------check buffer:---------------------------------");
+
+            MemoryStream stream = new MemoryStream(Buffer);
+            StreamReader SR = new StreamReader(stream);
+            MailMessage M = MailMessage.FromStream(SR);
+            foreach (var fileMessage in M.Attachments)
+            {
+                var FName = fileMessage.FName;
+                if (FName.StartsWith(FileFormatToken))
+                {
+                    FName = FName.Replace(FileFormatToken, string.Empty);
+                    if (FName.EndsWith("="))
+                        FName = FName.Substring(0, FName.Length - 1);
+
+                    FName = FName.
+                            Replace("=", "%").
+                            Replace("?", string.Empty);
+
+                    FName = Uri.UnescapeDataString(FName);
+                }
+                Console.WriteLine($"{FName} read succesfully");
+            }
+            Console.WriteLine("---------------------------------Process end---------------------------------");
             Console.ReadKey();
         }
     }
